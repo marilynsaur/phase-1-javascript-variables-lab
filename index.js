2e0680591f526b5ca35e215019fa6dd484b18c00
Marilyn Wren <Marilynwren64@yahoo.com>
2022-01-31 13:58:56 -0500

    commit tree 7070ee66d44b92470a344d92f1dcb3e0e53e8fe7

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+
+
+const companyName = 'Scuber';
+let companyCeo = 'Susan Smith';
+let mostProfitableNeighborhood = 'Chelsea';
+
+it('companyName', function () {
+expect(companyName).to.equal('Scuber');
+});
+
+
+    
+it('mostProfitableNeighborhood', function () {  
+expect(mostProfitableNeighborhood).to.equal('Chelsea');
+});
+
+
+ 
+it('companyCeo', function () { 
+expect(companyCeo).to.equal('Susan Smith');
+});
